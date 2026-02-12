c965de11a5977df68d92a3d248a4f9b5fd77f916
carl78-inf <carlos.salmeron@edu.uah.es>
2026-02-12 20:37:59 +0100

    función dibujar vaca

--- lab02-animacion/lab02-draw.py
+++ lab02-animacion/lab02-draw.py
@@ -85,22 +85,39 @@ arcade.draw_circle_outline(center_x=425, center_y=280, radius=10, color=arcade.c
 arcade.draw_circle_outline(center_x=375, center_y=280, radius=10, color=arcade.color.BLACK, border_width=5)"""
 
 #-----------------------------------------------------------------------------------------------------------------
-def dibujar_cuerpo(x: int, y: int, escala: float)->None:
+def dibujar_vaca(x: int, y: int, escala: float)->None:
     arcade.draw_circle_filled(center_x=x, center_y=y, radius=100*escala, color=arcade.color.WHITE)
-    arcade.draw_circle_outline(center_x=x, center_y=y, radius=100*escala, color=arcade.color.BLACK, border_width=10)
+    arcade.draw_circle_outline(center_x=x, center_y=y, radius=100*escala, color=arcade.color.BLACK, border_width=10*escala)
 
-    arcade.draw_lbwh_rectangle_filled(left=x-60*escala, bottom=195, width=30, height=50, color=arcade.color.WHITE)
-    arcade.draw_lbwh_rectangle_outline(left=x-60*escala, bottom=195, width=30, height=50, color=arcade.color.BLACK, border_width=10)
+    arcade.draw_lbwh_rectangle_filled(left=x-60*escala, bottom=y-105*escala, width=30*escala, height=50*escala, color=arcade.color.WHITE)
+    arcade.draw_lbwh_rectangle_outline(left=x-60*escala, bottom=y-105*escala, width=30*escala, height=50*escala, color=arcade.color.BLACK, border_width=10*escala)
 
-    arcade.draw_lbwh_rectangle_filled(left=430, bottom=y-105*escala, width=30*escala, height=50*escala, color=arcade.color.WHITE)
-    arcade.draw_lbwh_rectangle_outline(left=430, bottom=y-105*escala, width=30*escala, height=50*escala, color=arcade.color.BLACK, border_width=10)
+    arcade.draw_lbwh_rectangle_filled(left=x+30*escala, bottom=y-105*escala, width=30*escala, height=50*escala, color=arcade.color.WHITE)
+    arcade.draw_lbwh_rectangle_outline(left=x+30*escala, bottom=y-105*escala, width=30*escala, height=50*escala, color=arcade.color.BLACK, border_width=10*escala)
+    
+    arcade.draw_circle_filled(center_x=x-73*escala, center_y=y+30*escala, radius=20*escala, color=arcade.color.BLACK)
+    arcade.draw_circle_filled(center_x=x+78*escala, center_y=y-30*escala, radius=15*escala, color=arcade.color.BLACK)
+
+    arcade.draw_ellipse_filled(center_x=x-35*escala, center_y=y+45*escala, width=50*escala, height=30*escala, color=arcade.color.WHITE, tilt_angle=35)
+    arcade.draw_ellipse_outline(center_x=x-35*escala, center_y=y+45*escala, width=50*escala, height=30*escala, color=arcade.color.BLACK, tilt_angle=35, border_width=10*escala)
+
+    arcade.draw_ellipse_filled(center_x=x+35*escala, center_y=y+45*escala, width=50*escala, height=30*escala, color=arcade.color.WHITE, tilt_angle=-35)
+    arcade.draw_ellipse_outline(center_x=x+35*escala, center_y=y+45*escala, width=50*escala, height=30*escala, color=arcade.color.BLACK, tilt_angle=-35, border_width=10*escala)
+
+    arcade.draw_arc_filled(center_x=x, center_y=y-20*escala, width=100*escala, height=140*escala, color=arcade.color.WHITE, start_angle=0, end_angle=180)
+    arcade.draw_arc_outline(center_x=x, center_y=y-20*escala, width=100*escala, height=140*escala, color=arcade.color.BLACK, start_angle=0, end_angle=180, border_width=20*escala)
+
+    arcade.draw_ellipse_filled(center_x=x, center_y=y-20*escala, width=130*escala, height=50*escala, color=arcade.color.PINK)
+    arcade.draw_ellipse_outline(center_x=x, center_y=y-20*escala, width=130*escala, height=50*escala, color=arcade.color.BLACK, border_width=10*escala)
+
+    arcade.draw_circle_filled(center_x=x-15*escala, center_y=y+20*escala, radius=6*escala, color=arcade.color.BLACK)
+    arcade.draw_circle_filled(center_x=x+15*escala, center_y=y+20*escala, radius=6*escala, color=arcade.color.BLACK)
+
+    arcade.draw_circle_outline(center_x=x+25*escala, center_y=y-20*escala, radius=10*escala, color=arcade.color.BLACK, border_width=5*escala)
+    arcade.draw_circle_outline(center_x=x-25*escala, center_y=y-20*escala, radius=10*escala, color=arcade.color.BLACK, border_width=5*escala)
 
 
-def dibujar_vaca(x: int, y: int, escala: float) -> None:
-    pass
-
-
-dibujar_cuerpo(400,300,1)
+dibujar_vaca(500,200,0.5)
 
 #-----------------------------------------------------------------------------------------------------------------
 
