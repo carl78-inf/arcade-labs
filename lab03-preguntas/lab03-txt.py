b2fa4e84f1c4de8dfa60dead205028d62b9cf1a4
carl78-inf <carlos.salmeron@edu.uah.es>
2026-02-19 20:40:59 +0100

    ya funciona completamente

--- lab03-preguntas/lab03-txt.py
+++ lab03-preguntas/lab03-txt.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 import random
 
-def leer_opcion(mensaje)->int:
+def leer_opcion(mensaje, opciones)->int:
     """
     OBJ: Devuelve el entero correspondiente a la opción escogida
     """
-    pass
+    es_valido = False
+    while not es_valido:
+        try:
+            opcion = input(mensaje)
+            if opcion not in opciones: raise
+            else:
+                return opciones.index(opcion)
+        except:
+            print("La opción introducida no es válida")
 
 def orden_aleatorio(longitud:int)->list:
     lista_ordenada = list(range(longitud))
@@ -14,7 +22,7 @@ def orden_aleatorio(longitud:int)->list:
 def options(longitud:int)->list:
     opciones = []
     for i in range(longitud):
-        opciones += [chr(97 + i)+") "]
+        opciones += [chr(97 + i)]
     return opciones
 
 def extraer_preguntas(pregunta:str)->dict:
@@ -35,11 +43,27 @@ def juego(preguntas: str)->None:
         opciones = list_preguntas[i]["opciones"]
         letras = options(len(opciones))
         l = 0
+        respuestas = []
         for j in orden_aleatorio(len(opciones)): 
-            print(f"\t{letras[l]}{opciones[j].rstrip()}")
+            print(f"\t{letras[l]}) {opciones[j].rstrip()}")
+            respuestas += [opciones[j].rstrip()]
             l += 1
+        elegida = leer_opcion("Introduce la respuesta correcta: ", letras)
+        if respuestas[elegida] == list_preguntas[i]["correcta"]:
+            puntos += 1
+            print(f'\n¡Respuesta correcta!')
+            print('')
+        else:
+            print(f'\nRespuesta incorrecta')
+            print('')
         
-        
+    print(f'\nHas obtenido un total de: {puntos} puntos')
+    print('')
+    if puntos >= len(list_preguntas)/2:
+        print("Enhorabuena, has ganado")
+    else:
+        print("Lo siento, has perdido")
+    print('')
             
 
 
