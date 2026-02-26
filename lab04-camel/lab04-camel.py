eef0461a1e9d60dcc78280d0335e3922dfcca464
carl78-inf <carlos.salmeron@edu.uah.es>
2026-02-26 20:45:50 +0100

    Bloquear puertas

--- lab04-camel/lab04-camel.py
+++ lab04-camel/lab04-camel.py
@@ -1,11 +1,12 @@
 class Room:
-    def __init__(self, room = None, description = "Unknown", north = None, east = None, south = None, west = None):
+    def __init__(self, room = None, description = "Unknown", north = None, east = None, south = None, west = None, open = 0):
         self.room = room
         self.description = description
         self.north = north
         self.south = south
         self.east = east
         self.west = west
+        self.open = open
 
 if __name__ == '__main__':
     import json
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     #print(room_information)
 
     for room in room_information:
-        room_list.append(Room(room["number"], room["description"], room["north"], room["east"], room["south"], room["west"]))
+        room_list.append(Room(room["number"], room["description"], room["north"], room["east"], room["south"], room["west"], room["open"]))
 
     current_room = 0
     done = False
@@ -37,7 +38,8 @@ if __name__ == '__main__':
             next_room = room_list[current_room].west
         
         if(next_room["next_room"] != -1):
-            current_room = next_room["next_room"]
+            if room_list[next_room["next_room"]].open == 1: current_room = next_room["next_room"]
+            else: print("Esa puerta está bloqueada")
         else:
             if next_room["message"] != "": print(next_room["message"])
             else: print("No puedes ir por ahí")
