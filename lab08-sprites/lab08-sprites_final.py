791f70545c65f495f7e817712278b139f930c1ea
carl78-inf <carlos.salmeron@edu.uah.es>
2026-03-12 20:54:35 +0100

    coins recolected

--- lab08-sprites/lab08-sprites_final.py
+++ lab08-sprites/lab08-sprites_final.py
@@ -47,8 +47,8 @@ class Coin:
         self.sprite.center_x = random.randrange(SCREEN_WIDTH)
         self.sprite.center_y = random.randrange(SCREEN_HEIGHT)
         self.list.append(self.sprite)
-        self.change_x = random.randrange(5) - 2
-        self.change_y = random.randrange(5) - 2
+        self.change_x = random.randrange(3) +1
+        self.change_y = random.randrange(3) +1
     
     def draw(self):
         self.list.draw()
@@ -80,19 +80,33 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.ASH_GREY)
         self.player = Player(400, 300, 1, 0, 0)
         self.coin_list = []
-        for i in range(5):
+        self.good_sprite_list = arcade.SpriteList()
+        for i in range(20):
             self.coin_list.append(Coin(1, 3, 3))
+            self.good_sprite_list.append(self.coin_list[i].sprite)
+        self.contador = 0
+        self.tiempo = 0
     
     def on_draw(self):
         self.clear()
-        self.player.draw()
         for coin in self.coin_list:
             coin.draw()
+        self.player.draw()
+        hit_list = arcade.check_for_collision_with_list(self.player.sprite, self.good_sprite_list)
+        for coin in hit_list:
+            self.contador += 1
+            coin.remove_from_sprite_lists()
+        """hit_list = arcade.check_for_collision_with_list(self.player.sprite, self.bad_sprite_list)
+        for bad_sprite in hit_list:
+            self.contador -= 1"""
+        arcade.draw_text(text= self.contador, x = 50, y = 50, color= arcade.color.BLACK)
     
     def on_update(self, delta_time):
-        self.player.on_update()
+        self.tiempo += 1
+        if (self.tiempo %100 == 0):self.coin_list.append(Coin(1, 3, 3))
         for coin in self.coin_list:
             coin.on_update()
+        self.player.on_update()
     
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
